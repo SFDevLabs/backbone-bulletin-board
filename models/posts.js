5260bf457dc6d5a5b25e8977eb1b3e40577c3bbc
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-05-23 06:06:29 -0700

    models.

---
+++ models/posts.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose')
+    Schema = mongoose.Schema;
+   postsSchema = new mongoose.Schema({
+        title: { 'type': String, 'default': 'empty todo...' }
+       , body: { 'type': String, 'default': 'empty todo...' }
+       , createdAt : {type : Date, default : Date.now}
+       , user  : { type : Schema.ObjectId, ref : 'User' } 
+  });
+
+   postsSchema.statics = {
+  /**
+   * Find article by id
+   *
+   * @param {ObjectId} id
+   * @param {Function} cb
+   * @api public
+   */
+  load: function (id, cb) {
+    this.findOne({ _id : id })
+      .populate('user', 'username')
+      .exec(cb)
+  }
+}
+
+module.exports = mongoose.model('Post', postsSchema);
