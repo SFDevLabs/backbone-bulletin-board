793a8a2cb8a4fa2489f226767a20d2756ff6f17c
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-05-23 06:06:15 -0700

    Route Index

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+(function (exports) {
+  "use strict";
+  var mongoose = require('mongoose')
+    , crudUtils = require('../utils/crudUtils')
+    , post = mongoose.model('Post')
+    , users = require('../app/controller/users')
+
+  function index(req, res) {
+    res.render('index', { 
+        'title': 'Home'
+        , 'username':(req.user) ?  req.user.username: undefined
+        , 'userid':(req.user) ?  req.user._id: undefined
+      });
+  }
+  exports.init = function (app, auth, passport) {
+    app.get('/',index);
+    app.get('/login', users.login);
+    app.get('/signup', users.signup);
+    app.get('/logout', users.logout)
+    app.post('/users', users.create)
+    app.post('/users/session', passport.authenticate('local', {failureRedirect: '/login', failureFlash: 'Invalid email or password.'}), users.session)
+    crudUtils.initRoutesForModel({ 'app': app, 'model': post, auth: auth });
+  };
+}(exports));
