e8bb1ce9707bc378883d52993983248f42e52a36
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-05-23 06:05:43 -0700

    Config Files

---
+++ config/config.js
@@ -0,0 +1,36 @@
+
+module.exports = {
+    production: {
+      root: require('path').normalize(__dirname + '/..'),
+      app: {
+        name: 'Nodejs Express Mongoose Demo'
+      },
+      db: 'mongodb://localhost/backbone_passport',
+      facebook: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      }
+    }
+  , test: {
+
+    }
+  , dev: {
+
+    }
+}
---
+++ config/middlewares/authorization.js
@@ -0,0 +1,39 @@
+
+/*
+ *  Generic require login routing middleware
+ */
+
+exports.requiresLogin = function (req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.send('{status:Requires Authentication}', 404);
+  }
+  next()
+};
+
+/*
+ *  User authorizations routing middleware
+ */
+
+exports.user = {
+    hasAuthorization : function (req, res, next) {
+      if (req.profile.id != req.user.id) {
+        return res.send('{status:Requires Authentication}', 404);
+      }
+      next()
+    }
+}
+
+/*
+ *  Post authorizations routing middleware
+ */
+
+exports.post = {
+    hasAuthorization : function (req, res, next) {
+      console.log(req.post.user)
+      console.log(req.user.id)
+      if (req.post.user.id != req.user.id) {
+        return res.send('{status:Requires Authentication}', 404);
+      }
+      next()
+    }
+}
