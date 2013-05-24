550cd2874d331aa933de0712bfba8e292c00a11d
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-05-24 00:57:51 -0700

    favicon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ fs.readdirSync(models_path).forEach(function (file) {
   app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
