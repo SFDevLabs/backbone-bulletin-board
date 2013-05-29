88a0d93093fd3fe32a73d330aab2e96e8183392c
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-05-29 00:04:17 -0700

    adding in auth strategies.

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,15 @@
     app.get('/signup', users.signup);
     app.get('/logout', users.logout)
     app.post('/users', users.create)
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'email', 'user_about_me'], failureRedirect: '/login' }), users.signin)
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), users.authCallback)
+    app.get('/auth/github', passport.authenticate('github', { failureRedirect: '/login' }), users.signin)
+    app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), users.authCallback)
+    app.get('/auth/twitter', passport.authenticate('twitter', { failureRedirect: '/login' }), users.signin)
+    app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), users.authCallback)
+    app.get('/auth/google', passport.authenticate('google', { failureRedirect: '/login', scope: 'https://www.google.com/m8/feeds' }), users.signin)
+    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login', scope: 'https://www.google.com/m8/feeds' }), users.authCallback)
+
     app.post('/users/session', passport.authenticate('local', {failureRedirect: '/login', failureFlash: 'Invalid email or password.'}), users.session)
     crudUtils.initRoutesForModel({ 'app': app, 'model': post, auth: auth });
   };
